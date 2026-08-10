62962db93e30d1ebaed83c65a6eab47554b4de48
Joshua <joshuamoranvar@gmail.com>
2026-08-10 16:24:22 -0600

    fix: entrypoint root index.js simplificado para Pterodactyl

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-import { existsSync } from "node:fs";
-import { pathToFileURL } from "node:url";
-
-// Entry Point universal para Pterodactyl, Node.js y Bun
-if (existsSync("./dist/index.js")) {
-  import(pathToFileURL("./dist/index.js").href);
-} else {
-  try {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    await import("./src/index.ts");
-  } catch (err) {
-    console.error("[ENTRY] Error al iniciar el bot:", err);
-  }
-}
+import "./dist/index.js";
